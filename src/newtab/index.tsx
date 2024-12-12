6c62e42bcb6ba5dbdc045f266b1508ce47b76db2
Grant <zhenhua.zhao@antalpha.com>
2024-12-12 18:09:58 +0800

    feat: 依赖调试

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -1,11 +1,17 @@
 import "~style.css"
+import { TabLauncher } from "@clover-platform/launcher"
 
 document.title = chrome.i18n.getMessage("newTabTitle");
 
 const NewTab = () => {
-  return <div>
-    new tab
-  </div>
+  return <TabLauncher
+    defaultConfig={{
+      background: {
+        type: "image",
+        image: "http://localhost:4002/assets/wallpaper/default.jpg",
+      }
+    }}
+  />
 }
 
 export default NewTab;
